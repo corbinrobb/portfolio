3616fa559d40743ea09aab43145f43c6fe447863
Corbin Robb <corbinrobb@gmail.com>
2020-06-14 12:40:36 -0600

    Switch to react app

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Nav = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/resume">Resume</Link>
+    </nav>
+  );
+}
+
+export default Nav;
